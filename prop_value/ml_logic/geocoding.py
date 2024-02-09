d55d5737a9b725888b86207aa2d1339aaa901c4e
Julius Wilhelm <julius.wilhelm@me.com>
2024-02-09 15:28:53 +0100

    data preprocessing

--- prop_value/ml_logic/geocoding.py
+++ prop_value/ml_logic/geocoding.py
@@ -7,7 +7,7 @@ def get_citycode(postal_code: int) -> int:
     Get the city code based on the postal code.
     """
     #load pickle file with city codes and postal codes
-    file_path = '../../raw_data/citycodes.pkl'
+    file_path = '../../raw_data/codes_dict.pkl'
     with open(file_path, 'rb') as file:
         citycodes = pickle.load(file)
         assert citycodes is not None, "Citycodes are not loaded"
