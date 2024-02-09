0389f48648aca3d0536224ca30ad8ae96a93afb1
Anouchka Dierckx <anouchka.dierckx@gmail.com>
2024-02-09 02:26:47 +0100

    new notebook, updated with the correct preprocessor + implemented it into preprocessor.py

--- prop_value/ml_logic/preprocessor.py
+++ prop_value/ml_logic/preprocessor.py
@@ -18,7 +18,7 @@ import pickle
 import warnings
 warnings.filterwarnings('ignore')
 
-def clean_data(df_dvf: pd.DataFrame) -> pd.DataFrame:
+def clean_data(df_dvf: pd.DataFrame, percentile = 0.95) -> pd.DataFrame:
     """
     Clean raw data by
     - removing irrelevant columns
@@ -29,63 +29,116 @@ def clean_data(df_dvf: pd.DataFrame) -> pd.DataFrame:
     """
 
     #removing columns --> TO DO: should this be in params?
-    keep_col =['date_mutation',
-            'nature_mutation',
-            'valeur_fonciere',
-            'code_postal',
-            'code_commune',
-            'code_departement',
-            'nombre_lots',
-            'type_local',
-            'surface_reelle_bati',
-            'nombre_pieces_principales',
-            'longitude',
-            'latitude']
+    keep_col =['id_mutation',
+                'date_mutation',
+                'nature_mutation',
+                'valeur_fonciere',
+                'code_postal',
+                'code_commune',
+                'code_departement',
+                'type_local',
+                'surface_reelle_bati',
+                'nombre_pieces_principales',
+                'surface_terrain',
+                'longitude',
+                'latitude']
     df_dvf = df_dvf[keep_col]
 
     # TO DO : keep surface_terrain -> NaN => 0 (gives an indication if there is a garden or not)
 
     #translate the columns
-    df_dvf.columns = ['date', 'built', 'price', 'postal_code',
-                'city', 'region', 'number_of_units', 'property_type',
-                'living_area', 'number_of_rooms',
-                'longitude', 'latitude']
+    df_dvf.columns = ['id', 'date', 'built', 'price', 'postal_code',
+                    'city', 'region', 'property_type',
+                    'living_area', 'number_of_rooms', 'land_area',
+                    'longitude', 'latitude']
 
     #filter values inside columns:
         # 1.Keeping only regular sales,
         # 2.Filter for transactions for 1 unit,
         # 3.Only consider property types for living.
     df_useful = df_dvf[((df_dvf['built'] == "Vente") | (df_dvf['built'] == "Vente en l'état futur d'achèvement")) &
-                    ((df_dvf['number_of_units'] == 1) | (df_dvf['number_of_units'] == '1')) &
-                    ((df_dvf['property_type'] == 'Appartement') | (df_dvf['property_type'] == 'Maison'))]
+                        ((df_dvf['property_type'] == 'Appartement') | (df_dvf['property_type'] == 'Maison')  | (df_dvf['property_type'] == 'Maison')| (df_dvf['property_type'] == 'Dépendance'))]
 
     #translate values
     trans_dict_built = {'Vente' : 'built',
                         "Vente en l'état futur d'achèvement" : 'off-plan'}
     trans_dict_type = {'Appartement' : 'appartment',
-                    'Maison' : 'house'}
+                    'Maison' : 'house',
+                    'Dépendance' : 'dep'}
     df_useful= df_useful.replace({'built' : trans_dict_built,
-                                'property_type' : trans_dict_type})
+                             'property_type' : trans_dict_type})
 
-    #dropping the column with number of units (only 1s)
-    df_useful = df_useful.drop(columns='number_of_units')
+    #add new columns to eventually filter out sales with different units
+    ## 0. Creating the masks
+    mask_house = df_useful.property_type == 'house'
+    mask_app = df_useful.property_type == 'appartment'
+    mask_dep = df_useful.property_type == 'dep'
+    ## 1. find the number of houses per id
+    num_of_houses_df = df_useful[['id','property_type']][mask_house].groupby(by = 'id', as_index = False).count()
+    num_of_houses_df.columns = ['id', 'nb_of_house']
+    ## 2. find the number of appartments per id
+    num_of_app_df = df_useful[['id','property_type']][mask_app].groupby(by = 'id', as_index = False).count()
+    num_of_app_df.columns = ['id', 'nb_of_app']
 
+    ## 3. find the number of dep per id
+    num_of_dep_df = df_useful[['id','property_type']][mask_dep].groupby(by = 'id', as_index = False).count()
+    num_of_dep_df.columns = ['id', 'nb_of_dep']
 
-    #checking for NaN and duplicate values
-    df_useful = df_useful.dropna()
-    df_useful = df_useful.drop_duplicates()
+    ## 4. Merging 3 columns with df_useful
+    df_feat = df_useful.merge(num_of_houses_df, on = 'id', how = 'outer')
+    df_feat = df_feat.merge(num_of_app_df, on = 'id', how = 'outer')
+    df_feat = df_feat.merge(num_of_dep_df, on = 'id', how = 'outer')
 
-    #changing evething to the right type --> TO DO:should this go to params?
-    col_float = ['price', 'longitude', 'latitude', 'living_area', 'number_of_rooms']
+    # fill out the blanks for the 3 new columns + land_area
+    df_feat.fillna({'nb_of_house' : 0, 'nb_of_app' : 0, 'nb_of_dep' : 0, 'land_area' : 0}, inplace=True)
+
+    # remove lines with multiple houses or appartments in same sale_id
+    df_feat['nb_of_units'] = df_feat.nb_of_app+df_feat.nb_of_house
+    mask_units = (df_feat['nb_of_units'] == 1)
+    df_filtered = df_feat[mask_units]
+
+    #filter out the dependencies
+    filter_type = (df_filtered['property_type'] != 'dep')
+    df_filtered = df_filtered[filter_type]
+
+    # drop the useless columns
+    df_filtered_drop = df_filtered.drop(columns = ['nb_of_units', 'nb_of_app', 'nb_of_house'])
+
+    # drop na + duplicates (shouldn't be duplicates anymore at this point)
+    df_final = df_filtered_drop.drop(columns = ['land_area', 'id'])
+    df_clean = df_final.dropna()
+
+    # Create price per m2
+    df_clean[['price', 'living_area']] = df_clean[['price', 'living_area']].apply(lambda x: pd.to_numeric(x, errors='coerce').astype('float64'))
+
+    df_clean['price_per_m2'] = df_clean['price'] / df_clean['living_area']
+
+    #changing evething to the right type
+    col_float = ['price', 'longitude', 'latitude', 'living_area', 'price_per_m2' ]
     col_string = ['built','city', 'region','property_type']
     col_date = ['date']
-    col_int = ['postal_code']
+    col_int = ['postal_code', 'nb_of_dep', 'number_of_rooms']
     #formating data types
-    df_useful[col_float] = df_useful[col_float].apply(lambda x: pd.to_numeric(x, errors='coerce').astype('float64'))
-    df_useful[col_date]= df_useful[col_date].apply(lambda x: pd.to_datetime(x, errors='coerce'))
-    df_useful[col_int]= df_useful[col_int].apply(lambda x: pd.to_numeric(x, errors='coerce').astype('int64'))
+    df_clean[col_float] = df_clean[col_float].apply(lambda x: pd.to_numeric(x, errors='coerce').astype('float64'))
+    df_clean[col_date]= df_clean[col_date].apply(lambda x: pd.to_datetime(x, errors='coerce'))
+    df_clean[col_int]= df_clean[col_int].apply(lambda x: pd.to_numeric(x, errors='coerce').astype('int64'))
 
-    return df_useful
+    ##### FILTER OUT OUTLIERS WITH PRICE PER M2 ABOVE P95 (or other if mentioned)
+    # finding the percentile 95 for each postcode
+    df_quantile = df_clean[['postal_code', 'price_per_m2']].groupby(by = df_clean.postal_code, as_index=False).quantile(percentile)
+    df_quantile.columns = ['postal_code', 'p95']
+
+    # merge the df
+    df_clean_merged = df_quantile.merge(right = df_clean, on = 'postal_code')
+
+    # drop the outliers
+    mask_outliers = (df_clean_merged.price_per_m2 < df_clean_merged.p95)
+    df_without_outliers = df_clean_merged[mask_outliers]
+
+    # drop useless columns
+    df_without_outliers = df_without_outliers.drop(columns=['p95', 'price_per_m2'])
+
+    return df_without_outliers
 
 # def filter_data(max_price, max_area):
 
